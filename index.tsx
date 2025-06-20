f574b0eb828b6a87b347bda048a0bbfc9810c507
Desmondsol <51263712+DesmondSol@users.noreply.github.com>
2025-06-20 17:27:10 +0300

    edit

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { inject } from '@vercel/analytics';
+import { inject } from '@vercel/analytics/next';
 
 inject(); // Initialize Vercel Analytics
 
